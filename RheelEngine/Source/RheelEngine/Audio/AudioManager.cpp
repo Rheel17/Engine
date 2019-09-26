062da336a6e22de5646cfd418766ecbe305b53d7
Rheel17 <levivr97@gmail.com>
2019-09-26 12:51:47 +0200

    Removed optix from library dlls, started audio

---
+++ RheelEngine/Source/RheelEngine/Audio/AudioManager.cpp
@@ -0,0 +1,5 @@
+#include "AudioManager.h"
+
+namespace rheel {
+
+}
---
+++ RheelEngine/Source/RheelEngine/Audio/AudioManager.h
@@ -0,0 +1,13 @@
+#ifndef AUDIOMANAGER_H_
+#define AUDIOMANAGER_H_
+#include "../_common.h"
+
+namespace rheel {
+
+class RE_API AudioManager {
+
+};
+
+}
+
+#endif
