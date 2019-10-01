60f9322830b6e49e67dc739a8a39cfd62a2de256
Rheel17 <levivr97@gmail.com>
2019-10-01 23:24:06 +0200

    Clean up crosshair element

--- RheelEngine/Source/RheelEngine/UI/Elements/CrosshairElement.cpp
+++ RheelEngine/Source/RheelEngine/UI/Elements/CrosshairElement.cpp
@@ -12,13 +12,13 @@ void CrosshairElement::Draw(float dt) const {
 	const Bounds& bounds = GetBounds();
 
 	Bounds horizontal = {
-			bounds.x + (bounds.width - _size) / 2.0f,
-			bounds.y + (bounds.height - _size / 10.0f) / 2.0f,
-			_size, _size / 10.0f };
+			unsigned(bounds.x + (bounds.width - _size) / 2.0f),
+			unsigned(bounds.y + (bounds.height - _size / 10.0f) / 2.0f),
+			_size, unsigned(_size / 10.0f) };
 	Bounds vertical {
-			bounds.x + (bounds.width - _size / 10.0f) / 2.0f,
-			bounds.y + (bounds.height - _size) / 2.0f,
-			_size / 10.0f, _size };
+			unsigned(bounds.x + (bounds.width - _size / 10.0f) / 2.0f),
+			unsigned(bounds.y + (bounds.height - _size) / 2.0f),
+			unsigned(_size / 10.0f), _size };
 
 
 	_DrawColoredQuad(horizontal, { 1.0f, 1.0f, 1.0f, 0.2f });
