a601b9526cff0864d23a8522d7d7f4510d6054b1
Rheel17 <levivr97@gmail.com>
2019-10-04 12:27:17 +0200

    Corrected scale for player start

--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -15,7 +15,7 @@ public:
 				auto camera = Parent().GetCamera("main_camera");
 				Object& object = Parent().AddObject("ball", camera->Position());
 				object.GetComponent<RigidBodyComponent>()->ApplyImpulse(
-						quat(camera->Rotation()) * vec4(0, 0, -1000000, 0));
+						quat(camera->Rotation()) * vec4(0, 0, -100, 0));
 			}
 		}
 	}
@@ -37,15 +37,15 @@ public:
 static Blueprint createCubeBlueprint() {
 	Blueprint blueprint("cube");
 
-	ModelResource& model = ModelResource::Box({ 1, 1, 1 });
+	ModelResource& model = ModelResource::Box({ 0.5f, 0.5f, 0.5f });
 
 	auto& modelRenderComponent = blueprint.AddComponent<ModelRenderComponent>();
 	modelRenderComponent.SetMaterial(Material({ 0.9f, 0.6f, 0.2f, 1.0f }, 0.7f, 0.5f));
 	modelRenderComponent.SetModel(model);
 
 	auto& rigidBodyComponent = blueprint.AddComponent<RigidBodyComponent>();
-	rigidBodyComponent.SetShape(PhysicsShape::Box({ 1.0f, 1.0f, 1.0f }));
-	rigidBodyComponent.SetMass(10000.0f);
+	rigidBodyComponent.SetShape(PhysicsShape::Box({ 0.5f, 0.5f, 0.5f }));
+	rigidBodyComponent.SetMass(5.0f);
 	rigidBodyComponent.SetBounciness(0.05f);
 
 	blueprint.AddComponent<RemoveComponent>();
@@ -56,14 +56,14 @@ static Blueprint createCubeBlueprint() {
 static Blueprint createFloorBlueprint() {
 	Blueprint blueprint("floor");
 
-	ModelResource& model = ModelResource::Box({ 30.0f, 1.0f, 30.0f });
+	ModelResource& model = ModelResource::Box({ 15.0f, 0.5f, 15.0f });
 
 	auto& modelRenderComponent = blueprint.AddComponent<ModelRenderComponent>();
 	modelRenderComponent.SetMaterial(Material({ 0.6f, 0.7f, 1.0f, 1.0f }, 0.7f, 0.2f));
 	modelRenderComponent.SetModel(model);
 
 	auto& rigidBodyComponent = blueprint.AddComponent<RigidBodyComponent>();
-	rigidBodyComponent.SetShape(PhysicsShape::Box({ 30.0f, 1.0f, 30.0f }));
+	rigidBodyComponent.SetShape(PhysicsShape::Box({ 15.0f, 0.5f, 15.0f }));
 
 	return blueprint;
 }
@@ -75,12 +75,12 @@ static Blueprint createBallBlueprint() {
 
 	auto& modelRenderComponent = blueprint.AddComponent<ModelRenderComponent>();
 	modelRenderComponent.SetMaterial(Material({ 1.0f, 1.0f, 1.0f, 1.0f }, 0.7f, 0.05f));
-	modelRenderComponent.SetScale(1.5f);
+	modelRenderComponent.SetScale(0.25f);
 	modelRenderComponent.SetModel(model);
 
 	auto& rigidBodyComponent = blueprint.AddComponent<RigidBodyComponent>();
-	rigidBodyComponent.SetShape(PhysicsShape::Sphere(1.5f));
-	rigidBodyComponent.SetMass(18000.0f);
+	rigidBodyComponent.SetShape(PhysicsShape::Sphere(0.25f));
+	rigidBodyComponent.SetMass(3.0f);
 	rigidBodyComponent.SetBounciness(0.3f);
 
 	blueprint.AddComponent<RemoveComponent>();
@@ -91,14 +91,14 @@ static Blueprint createBallBlueprint() {
 static Blueprint createRampBlueprint() {
 	Blueprint blueprint("ramp");
 
-	ModelResource& model = ModelResource::Box({ 8.0f, 1.0f, 10.0f });
+	ModelResource& model = ModelResource::Box({ 4.0f, 0.1f, 5.0f });
 
 	auto& modelRenderComponent = blueprint.AddComponent<ModelRenderComponent>();
 	modelRenderComponent.SetMaterial(Material({ 0.3f, 0.7f, 0.4f, 1.0f }, 0.7f, 0.2f));
 	modelRenderComponent.SetModel(model);
 
 	auto& rigidBodyComponent = blueprint.AddComponent<RigidBodyComponent>();
-	rigidBodyComponent.SetShape(PhysicsShape::Box({ 8.0f, 1.0f, 10.0f }));
+	rigidBodyComponent.SetShape(PhysicsShape::Box({ 4.0f, 0.1f, 5.0f }));
 
 	return blueprint;
 }
@@ -106,14 +106,14 @@ static Blueprint createRampBlueprint() {
 static Blueprint createPlayerBlueprint() {
 	Blueprint blueprint("player");
 
-	ModelResource& model = ModelResource::Capsule(0.7f, 0.4f);
+	ModelResource& model = ModelResource::Capsule(0.4f, 1.0f);
 
 	auto& modelRenderComponent = blueprint.AddComponent<ModelRenderComponent>();
 	modelRenderComponent.SetMaterial(Material({ 1.0f, 0.9f, 0.8f, 1.0f }, 0.7f, 0.4f));
 	modelRenderComponent.SetModel(model);
 
 	auto& rigidBodyComponent = blueprint.AddComponent<RigidBodyComponent>();
-	rigidBodyComponent.SetShape(PhysicsShape::Capsule(0.7f, 0.4f));
+	rigidBodyComponent.SetShape(PhysicsShape::Capsule(0.4f, 1.0f));
 	rigidBodyComponent.SetMass(80.0f);
 	rigidBodyComponent.SetBounciness(0.0f);
 
@@ -136,17 +136,17 @@ static SceneDescription createSceneDescription() {
 
 	for (int i = -2; i <= 2; i++) {
 		for (int j = 0; j < 5; j++) {
-			description.AddObject("cube", { 2.1f * i, 2.1f * j, 2.1f * i });
+			description.AddObject("cube", { 1.1f * i, 1.1f * j + 0.5f, 1.1f * i });
 		}
 	}
 
-	description.AddObject("floor", { 0, -2, 0 });
-	description.AddObject("ramp", { -16, 5, 0 }, quat(vec3(0, 0, -0.6f)));
-	description.AddObject("ramp", { 16, 4, 0 }, quat(vec3(0, 0, 0.8f)));
-	description.AddObject("player", { 0, 4, 10 });
+	description.AddObject("floor", { 0, -0.25, 0 });
+	description.AddObject("ramp", { -8, 3, 0 }, quat(vec3(0, 0, -0.6f)));
+	description.AddObject("ramp", { 8, 2, 0 }, quat(vec3(0, 0, 0.8f)));
+	description.AddObject("player", { 0, 2, 10 });
 
 	description.AddLight("main_light", DirectionalLight({ 1, 1, 1, 1 }, { 0.2f, -2.0f, -1.0f }), 100.0f);
-	description.AddCamera("main_camera", 75.0f, 0.01f, 100.0f, { -25, 15, 0 }, { -0.5f, -M_PI / 2.0f, 0.0f });
+	description.AddCamera("main_camera", 75.0f, 0.01f, 100.0f, { -12, 7.5f, 0 }, { -0.5f, -M_PI / 2.0f, 0.0f });
 
 	return description;
 }
