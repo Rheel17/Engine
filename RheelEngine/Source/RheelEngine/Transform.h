c230963262d828000fd5cdb77872980c0a5127a0
Rheel17 <levivr97@gmail.com>
2019-10-13 01:21:39 +0200

    Continued on transform header

--- RheelEngine/Source/RheelEngine/Transform.h
+++ RheelEngine/Source/RheelEngine/Transform.h
@@ -15,9 +15,62 @@ public:
 			quat rotation = { 1.0f, 0.0f, 0.0f, 0.0f },
 			vec3 scale = { 1.0f, 1.0f, 1.0f });
 
-	vec3 translation;
-	quat rotation;
-	vec3 scale;
+	/**
+	 * Returns the translation component of this transform.
+	 */
+	const vec3& GetTranslation() const;
+
+	/**
+	 * Sets the translation of this transform to the new vector.
+	 */
+	void SetTranslation(vec3 translation);
+
+	/**
+	 * Moves the translation component of this transform with the vector.
+	 *
+	 * Note: this only updates the internal translation. The move will not take
+	 * the rotation or scale into account. Calling this method is equivalent to
+	 * SetTranslation(GetTranslation() + vec).
+	 */
+	void Move(vec3 vec);
+
+	/**
+	 * Returns the rotation component of this transform.
+	 */
+	const quat& GetRotation() const;
+
+	/**
+	 * Sets the rotation of this transform to the new quaternion.
+	 */
+	void SetRotation(quat rotation);
+
+	/**
+	 * Rotates the rotational component of this transform with the quaternion.
+	 *
+	 * Note: this only updates the internal rotation. The rotation will not take
+	 * the translation or scale into account. Calling this method is equivalent
+	 * to SetRotation(). // TODO left or right multiplication?
+	 */
+	void Rotate(quat rotation);
+
+	/**
+	 * Returns the scale component of this transform.
+	 */
+	const vec3& GetScale() const;
+
+	/**
+	 * Returns the resulting matrix of this transform.
+	 */
+	const mat4& GetMatrix() const;
+
+private:
+	vec3 _translation;
+	quat _rotation;
+	vec3 _scale;
+
+	mat4 _matrix;
+	bool _matrix_dirty;
+
 };
 
 }
