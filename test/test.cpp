4957b050eeb4ae6230b2979c0f6a66290c6fdc24
Rheel17 <levivr97@gmail.com>
2020-04-14 13:54:27 +0200

    Add googletest back

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) Levi van Rheenen. All rights reserved.
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[]) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
