59d351afcc2b62553c945a4dfb9006f293e7e38c
Rheel17 <levivr97@gmail.com>
2020-04-19 16:40:13 +0200

    Added Animator

---
+++ src/RheelEngine/Animator/Animator.cpp
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2020 Levi van Rheenen
+ */
+#include "Animator.h"
+
+namespace rheel {
+
+void Animator::AddClip(const std::string& name, Clip clip) {
+	PlayState state = _play_state;
+
+	if (_play_state != PlayState::STOPPED) {
+		RemoveClip(name);
+	}
+
+	_clips[name] = std::make_unique<Clip>(std::move(clip));
+
+	switch (state) {
+		case PlayState::STOPPED:
+			break;
+		case PlayState::PLAYING:
+			Play(name);
+			break;
+		case PlayState::LOOPING:
+			Loop(name);
+			break;
+	}
+}
+
+void Animator::RemoveClip(const std::string& name) {
+	if (name == _current_clip) {
+		Stop();
+	}
+
+	_clips.erase(name);
+}
+
+Clip& Animator::GetClip(const std::string& name) {
+	return *_clips.find(name)->second;
+}
+
+const Clip& Animator::GetClip(const std::string& name) const {
+	return *_clips.find(name)->second;
+}
+
+void Animator::Play(const std::string& name) {
+	if (_play_state != PlayState::STOPPED) {
+		Stop();
+	}
+
+	_current_clip = name;
+	_play_state = PlayState::PLAYING;
+	_time = 0.0f;
+}
+
+void Animator::Loop(const std::string& name) {
+	if (_play_state != PlayState::STOPPED) {
+		Stop();
+	}
+
+	_current_clip = name;
+	_play_state = PlayState::LOOPING;
+	_time = 0.0f;
+}
+
+void Animator::Stop() {
+	_current_clip.clear();
+	_play_state = PlayState::STOPPED;
+}
+
+const std::string& Animator::GetCurrentClip() {
+	return _current_clip;
+}
+
+Animator::PlayState Animator::GetCurrentPlayState() {
+	return _play_state;
+}
+
+}
---
+++ src/RheelEngine/Animator/Animator.h
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2020 Levi van Rheenen
+ */
+#ifndef RHEELENGINE_ANIMATOR_H
+#define RHEELENGINE_ANIMATOR_H
+#include "../_common.h"
+
+#include "Clip.h"
+
+namespace rheel {
+
+class RE_API Animator {
+
+public:
+	enum class PlayState {
+		STOPPED, PLAYING, LOOPING
+	};
+
+public:
+	/**
+	 * Adds a clip to the animator. The name should be used to reference the
+	 * clip in the future. If a clip with that name already exists, it will
+	 * first be removed. If that clip is playing, it will be stopped and this
+	 * clip will be either played or looped, depending on the state of the
+	 * removed clip.
+	 *
+	 * Note: the empty string ("") is not a valid clip name. When it is given,
+	 * this function will throw an error.
+	 */
+	void AddClip(const std::string& name, Clip clip);
+
+	/**
+	 * Removes the clip from the animator. If the clip is currently playing, it
+	 * will first be stopped.
+	 */
+	void RemoveClip(const std::string& name);
+
+	/**
+	 * Returns the clip object with the given name. If the name does not have a
+	 * clip registered to it, this will fail.
+	 */
+	Clip& GetClip(const std::string& name);
+
+	/**
+	 * Returns the clip object with the given name. If the name does not have a
+	 * clip registered to it, this will fail.
+	 */
+	const Clip& GetClip(const std::string& name) const;
+
+	/**
+	 * Plays an animation clip once. Any other previous animation will be
+	 * stopped. If the given name is the name of the currently playing clip, it
+	 * will be restarted.
+	 */
+	void Play(const std::string& name);
+
+	/**
+	 * Loops an animation clip. Any other previous animation will be
+	 * stopped. If the given name is the name of the currently playing clip, it
+	 * will be restarted.
+	 */
+	void Loop(const std::string& name);
+
+	/**
+	 * Stops the current animation clip
+	 */
+	void Stop();
+
+	/**
+	 * Returns the name of the current clip. This will return the empty string
+	 * if no clip is currently playing.
+	 */
+	const std::string& GetCurrentClip();
+
+	/**
+	 * Returns the current playing state.
+	 */
+	PlayState GetCurrentPlayState();
+
+private:
+	std::unordered_map<std::string, std::unique_ptr<Clip>> _clips;
+
+	std::string _current_clip;
+	PlayState _play_state;
+	float _time;
+
+};
+
+}
+
+#endif
