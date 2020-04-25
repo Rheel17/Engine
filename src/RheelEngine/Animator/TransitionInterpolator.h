ade7ecd0bf749a984407a5f63a1f6c724d04f3f9
Rheel17 <levivr97@gmail.com>
2020-04-25 23:51:13 +0200

    Allowing custom Transition Interpolators

--- src/RheelEngine/Animator/TransitionInterpolator.h
+++ src/RheelEngine/Animator/TransitionInterpolator.h
@@ -16,10 +16,10 @@ class TransitionInterpolator : public Interpolator<V> {
 public:
 	virtual ~TransitionInterpolator() = default;
 
-protected:
 	explicit TransitionInterpolator(const Transition& transition) :
 			_transition(transition) {}
 
+protected:
 	V GetValue(float t) const override {
 		auto before = --this->points.upper_bound(t);
 		auto after = this->points.upper_bound(t);
