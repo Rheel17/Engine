28f8b10a9aed921166d3be47e14fcfa04815cfdf
Rheel17 <levivr97@gmail.com>
2020-05-26 20:55:14 +0200

    Updating tests after C++20

--- test/test_Encoding.cpp
+++ test/test_Encoding.cpp
@@ -9,12 +9,12 @@ using namespace rheel;
 
 TEST(Encoding, CodePointToUtf8) {
 	EXPECT_EQ(Encoding::CodePointToUtf8(0x00),    std::string(1, 0x00));
-	EXPECT_EQ(Encoding::CodePointToUtf8(0x24),    u8"\u0024");
-	EXPECT_EQ(Encoding::CodePointToUtf8(0xa2),    u8"\u00a2");
-	EXPECT_EQ(Encoding::CodePointToUtf8(0x939),   u8"\u0939");
-	EXPECT_EQ(Encoding::CodePointToUtf8(0x20ac),  u8"\u20ac");
-	EXPECT_EQ(Encoding::CodePointToUtf8(0xd55c),  u8"\ud55c");
-	EXPECT_EQ(Encoding::CodePointToUtf8(0x10348), u8"\U00010348");
+	EXPECT_EQ(Encoding::CodePointToUtf8(0x24),    "\u0024");
+	EXPECT_EQ(Encoding::CodePointToUtf8(0xa2),    "\u00a2");
+	EXPECT_EQ(Encoding::CodePointToUtf8(0x939),   "\u0939");
+	EXPECT_EQ(Encoding::CodePointToUtf8(0x20ac),  "\u20ac");
+	EXPECT_EQ(Encoding::CodePointToUtf8(0xd55c),  "\ud55c");
+	EXPECT_EQ(Encoding::CodePointToUtf8(0x10348), "\U00010348");
 
 	EXPECT_EQ(Encoding::CodePointToUtf8(-1), "");
 	EXPECT_EQ(Encoding::CodePointToUtf8(0x110000), "");
@@ -22,20 +22,20 @@ TEST(Encoding, CodePointToUtf8) {
 }
 
 TEST(Encoding, Utf8ToCodePoint) {
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\u0000"), 0x00);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\u0024"), 0x24);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\u00a2"), 0xa2);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\u0939"), 0x939);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\u20ac"), 0x20ac);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\ud55c"), 0xd55c);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint(u8"\U00010348"), 0x10348);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\u0000")), 0x00);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\u0024")), 0x24);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\u00a2")), 0xa2);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\u0939")), 0x939);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\u20ac")), 0x20ac);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\ud55c")), 0xd55c);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\U00010348")), 0x10348);
 
-	EXPECT_EQ(Encoding::Utf8ToCodePoint("\xff"), -1);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint("\x8f"), -1);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint("\xf1"), -1);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint("\xdf\x00"), -1);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint("\xef\xbf\x00"), -1);
-	EXPECT_EQ(Encoding::Utf8ToCodePoint("\xf0\xbf\xbf\x00"), -1);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\xff")), -1);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\x8f")), -1);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\xf1")), -1);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\xdf\x00")), -1);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\xef\xbf\x00")), -1);
+	EXPECT_EQ(int32_t(Encoding::Utf8ToCodePoint("\xf0\xbf\xbf\x00")), -1);
 }
 
 TEST(Encoding, Utf8Length) {
